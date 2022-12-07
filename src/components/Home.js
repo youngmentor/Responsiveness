77da12504235997dd1ec589c16eb5e8d2566385a
youngmentor <olaleresuliton43@gmail.com>
2022-12-06 22:48:00 -0800

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import './Home/Header/Header.css'
+import Dashboard from './Home/Dashboard/Dashboard';
+
+const Home = () => {
+    return (<div className='Home'>
+        <Dashboard />
+    </div>)
+}
+
+export default Home
---
+++ src/components/Home/Header/Header.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import './Header.css'
+import { FaTimes, FaBeer } from 'react-icons/fa'
+import { FiAlignJustify } from 'react-icons/fi'
+import Nav from './nav'
+const Header = ({Profile}) => {
+    const [toggle, settoggle] = useState(true)
+    const handletoggle = () => { settoggle(!toggle) }
+
+    const FiAlignJustif = (<FiAlignJustify onClick={handletoggle} />)
+    const FaTime = (<div>
+        <FaTimes  onClick={handletoggle} />
+    </div>)
+    return (
+        <div className="Header-Cointainer">
+            <div className="Header-Wraper">
+                <div>LOGO</div>
+                <Nav />
+                <div className='Burger'>
+                    {toggle ? FiAlignJustif : FaTime}
+                </div>
+
+            </div>
+            <div>
+               {!toggle? Profile:''}
+            </div>
+        </div>
+    )
+}
+
+export default Header
